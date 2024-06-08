44ba333f25a9ca1cef3a80d51961d0ceda7cbb5a
jasminemahfouz <jasmine12mahfouz@gmail.com>
2024-06-08 10:16:57 +0200

    model

--- End_Week/LASTFRONT_YASMIN_MAHFOUZ/src/app/pages/dashboard/dashboard.component.ts
+++ End_Week/LASTFRONT_YASMIN_MAHFOUZ/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { iUser } from '../../Models/i-user';
 import { iMovie } from '../../Models/i-movie';
 import { MovieService } from '../../movie.service';
 import { iMoviePreferiti } from '../../Models/i-movie-preferiti';
 import { MoviePreferitoService } from '../../movie-preferito.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,16 +13,20 @@ import { MoviePreferitoService } from '../../movie-preferito.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild('highlightModal') highlightModal!: TemplateRef<any>;
 
   user!: iUser;
   users: iUser[] = [];
   movies: iMovie[] = [];
   favorites: iMoviePreferiti[] = [];
+  highlightedMovie: iMovie | null = null;
+  isModalOpen: boolean = false;
 
   constructor(
     private authSvc: AuthService,
     private movieSvc: MovieService,
-    private moviePreferitoSvc: MoviePreferitoService
+    private moviePreferitoSvc: MoviePreferitoService,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit() {
@@ -74,7 +79,7 @@ export class DashboardComponent implements OnInit {
     const isAlreadyFavorite = this.favorites.some(fav => fav.movie.id === movie.id);
     if (isAlreadyFavorite) {
       console.log('Il film è già nei preferiti');
-      return; // Esci dalla funzione se il film è già nei preferiti
+      return;
     }
     const favorite: Partial<iMoviePreferiti> = {
       userId: this.user.id,
@@ -103,6 +108,7 @@ export class DashboardComponent implements OnInit {
       });
     }
   }
+
   usersVisible: boolean = false;
   moviesVisible: boolean = false;
 
@@ -113,7 +119,22 @@ export class DashboardComponent implements OnInit {
   toggleMovies() {
     this.moviesVisible = !this.moviesVisible;
   }
+
   isFavorite(movie: iMovie): boolean {
     return this.favorites.some(fav => fav.movie.id === movie.id);
+  }
+
+  openModal(movie: iMovie) {
+    this.highlightedMovie = movie;
+    this.isModalOpen = true;
+    this.modalService.open(this.highlightModal).result.finally(() => {
+      this.isModalOpen = false; 
+    });
+  }
+
+  closeModal(modal: any) {
+    modal.dismiss();
+    this.highlightedMovie = null;
+    this.isModalOpen = false;
   }
 }
