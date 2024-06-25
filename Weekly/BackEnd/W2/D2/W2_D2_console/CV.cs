1086b510a04ad332482c152ad713a9fd57795377
jasminemahfouz <jasmine12mahfouz@gmail.com>
2024-06-25 10:18:59 +0200

    esericizio day2 backend

---
+++ Weekly/BackEnd/W2/D2/W2_D2_console/CV.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace W2_D2_console
+{
+        public class CV
+        {
+            public InformazioniPersonali InformazioniPersonali { get; set; }
+            public List<Studi> StudiEffettuati { get; set; }
+            public List<Impiego> Impieghi { get; set; }
+
+            public CV()
+            {
+                StudiEffettuati = new List<Studi>();
+                Impieghi = new List<Impiego>();
+            }
+        }
+
+        public class InformazioniPersonali
+        {
+            public string Nome { get; set; }
+            public string Cognome { get; set; }
+            public string Telefono { get; set; }
+            public string Email { get; set; }
+        }
+
+        public class Studi
+        {
+            public string Qualifica { get; set; }
+            public string Istituto { get; set; }
+            public string Tipo { get; set; }
+            public DateTime Dal { get; set; }
+            public DateTime Al { get; set; }
+        }
+
+        public class Impiego
+        {
+            public Esperienza Esperienza { get; set; }
+        }
+
+        public class Esperienza
+        {
+            public string Azienda { get; set; }
+            public string JobTitle { get; set; }
+            public DateTime Dal { get; set; }
+            public DateTime Al { get; set; }
+            public string Descrizione { get; set; }
+            public string Compiti { get; set; }
+        }
+    }
+
---
+++ Weekly/BackEnd/W2/D2/W2_D2_console/Program.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+using W2_D2_console;
+
+namespace W2_D2_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CV cv = new CV
+            {
+                InformazioniPersonali = new InformazioniPersonali
+                {
+                    Nome = "Marco",
+                    Cognome = "Puccio",
+                    Telefono = "+393288317446",
+                    Email = "info@marcopuccio.com"
+                }
+            };
+
+            cv.StudiEffettuati.Add(new Studi
+            {
+                Istituto = "UniPa",
+                Qualifica = "",
+                Tipo = "",
+                Dal = new DateTime(2016, 9, 12),
+                Al = new DateTime(2019, 7, 22)
+            });
+
+            cv.StudiEffettuati.Add(new Studi
+            {
+                Istituto = "Microsoft Center Milan",
+                Qualifica = "",
+                Tipo = "Specializzazione sviluppo software",
+                Dal = new DateTime(2016, 9, 12),
+                Al = new DateTime(2019, 7, 22)
+            });
+
+            cv.Impieghi.Add(new Impiego
+            {
+                Esperienza = new Esperienza
+                {
+                    Azienda = "Libero Professionista",
+                    JobTitle = "IT Manager",
+                    Descrizione = "",
+                    Compiti = "Sviluppatore software",
+                    Dal = new DateTime(2016, 9, 12),
+                    Al = new DateTime(2016, 9, 12)
+                }
+            });
+
+            cv.Impieghi.Add(new Impiego
+            {
+                Esperienza = new Esperienza
+                {
+                    Azienda = "Libero Professionista",
+                    JobTitle = "Docenza e Formazione",
+                    Descrizione = "",
+                    Compiti = "Docente e Formatore",
+                    Dal = new DateTime(2016, 9, 12),
+                    Al = new DateTime(2016, 9, 12)
+                }
+            });
+
+            StampaDettagliCVSuSchermo(cv);
+        }
+
+        static void StampaDettagliCVSuSchermo(CV cv)
+        {
+            Console.WriteLine($"CV di {cv.InformazioniPersonali.Nome} {cv.InformazioniPersonali.Cognome}");
+            Console.WriteLine("++++ INIZIO Informazioni Personali: ++++");
+            Console.WriteLine($"Nome: {cv.InformazioniPersonali.Nome}");
+            Console.WriteLine($"Cognome: {cv.InformazioniPersonali.Cognome}");
+            Console.WriteLine($"Email: {cv.InformazioniPersonali.Email}");
+            Console.WriteLine($"Telefono: {cv.InformazioniPersonali.Telefono}");
+            Console.WriteLine("++++ FINE Informazioni Personali: ++++");
+
+            Console.WriteLine("++++ INIZIO Studi e Formazione: ++++");
+            foreach (var studio in cv.StudiEffettuati)
+            {
+                Console.WriteLine($"Istituto: {studio.Istituto}");
+                Console.WriteLine($"Qualifica: {studio.Qualifica}");
+                Console.WriteLine($"Tipo: {studio.Tipo}");
+                Console.WriteLine($"Dal: {studio.Dal} al {studio.Al}");
+            }
+            Console.WriteLine("++++ FINE Studi e Formazione: ++++");
+
+            Console.WriteLine("++++ INIZIO Esperienze professionali: ++++");
+            foreach (var impiego in cv.Impieghi)
+            {
+                Console.WriteLine($"Presso: {impiego.Esperienza.Azienda}");
+                Console.WriteLine($"Tipo di lavoro: {impiego.Esperienza.JobTitle}");
+                Console.WriteLine($"Compito: {impiego.Esperienza.Compiti}");
+                Console.WriteLine($"Dal: {impiego.Esperienza.Dal} al {impiego.Esperienza.Al}");
+            }
+            Console.WriteLine("++++ FINE Esperienze professionali: ++++");
+        }
+    }
+}
